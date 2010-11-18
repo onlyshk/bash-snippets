d7a314f794d8a7c10a1453c1aa40caf78ae1b076
Kuleshov Alexander <kuleshovmail@gmail.com>
2010-11-18 19:31:24 +0000

    deleted some files

--- all_file_lower_case.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in * ; do mv $i `echo $i | tr [A-Z] [a-z]` ; done
--- git_all_add.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Enter commit message: " comment;
-git add .
-echo "All files added"
-git commit -m $commit
-echo "Commit done"
-git push
-echo "Pushed"
